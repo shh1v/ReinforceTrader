02574eb997b50a8e3fca7f5abba84bf414c6feb7
shh1v <shivpatelhw@gmail.com>
2025-08-22 23:53:46 -0700

    Implement standalone ticker data download in RawDataLoader

--- reinforcetrader/data_pipeline.py
+++ reinforcetrader/data_pipeline.py
@@ -10,17 +10,29 @@ from ta.momentum import RSIIndicator
 
 
 class RawDataLoader:
-    def __init__(self, start_date: str, end_date: str, index: str='DJI'):
+    def __init__(self, start_date: str, end_date: str, tickers=[], index: str=''):
         # Store the start and end dates of data to be downloaded/load from cache
         self._start_date = start_date
         self._end_date = end_date
         self._index = index
 
-        # Fetch all the tickers in S&P 500
-        tickers = self._fetch_tickers(index=self._index)
+        # Make sure there is no conflict between tickers and index
+        if tickers and index:
+            raise ValueError('Tickers and index cannot be provided simultaneously.')
+        elif not tickers and not index:
+            raise ValueError('Either tickers or index must be provided.')
+        
+        # If tickers are provided, do load them from cache
+        load_from_cache = False
+                
+        # Fetch all the tickers in index if tickers are not provided
+        if not tickers:
+            # Prefer loading from cache to avoid API calls
+            load_from_cache = True
+            tickers = self._fetch_tickers(index=self._index)
 
         # Load all the ticker price and volume data
-        self._hist_data = self._load_hist_prices(tickers)
+        self._hist_data = self._load_hist_prices(tickers, load_from_cache=load_from_cache)
             
     def _fetch_tickers(self, index: str='DJI') -> list:
         if index == 'DJI':
@@ -73,7 +85,7 @@ class RawDataLoader:
 
         return data
 
-    def _load_hist_prices(self, tickers: list, cache_path: str='data/raw') -> pd.DataFrame:
+    def _load_hist_prices(self, tickers: list, load_from_cache, cache_path: str='data/raw') -> pd.DataFrame:
 
         # Build cache directory and file path
         cache_dir = Path(cache_path)
@@ -81,12 +93,12 @@ class RawDataLoader:
         file_path = cache_dir / f"{self._index}_tickers_data_{self._start_date}_{self._end_date}.csv"
 
         # If cached file exists, load and return
-        if file_path.exists():
+        if load_from_cache and file_path.exists():
             print(f"Loading cached data from {file_path}")
             return pd.read_csv(file_path, header=[0, 1], index_col=0, parse_dates=True)
-
+            
         print(f'Downloading from yfinance as cached data does not exist in {cache_path}')
-        return self._download_hist_prices(tickers, save=True, save_path=file_path)
+        return self._download_hist_prices(tickers, save=load_from_cache, save_path=file_path)
 
     def get_hist_prices(self, tickers: list=[]):
         # Only return selected columns
